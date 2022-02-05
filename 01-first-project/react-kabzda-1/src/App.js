2073c334b0cedf8e11691d062070a9351892bdd7
AlexeyVarenko <alexey_varenko@mail.ru>
2022-02-05 19:23:57 +0300

    make changes

---
+++ 01-first-project/react-kabzda-1/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './Header.js';
+import Technologies from './Technologies.js';
+
+const App = () => {
+
+  return (
+
+    <div>
+      <Header />
+      <Technologies/>
+    </div>
+  );
+}
+
+
+
+
+
+export default App;
