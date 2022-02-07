b3e59ed70ecfa6796d05c9e846f6d0f933275fec
AlexeyVarenko <alexey_varenko@mail.ru>
2022-02-07 14:03:36 +0300

    make changes

--- 01-first-project/react-kabzda-1/src/App.js
+++ 01-first-project/react-kabzda-1/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-import Header from './Header.js';
-import Technologies from './Technologies.js';
+import Footer from './Footer';
+import Header from './Header';
+import Technologies from './Technologies';
+
 
 const App = () => {
 
   return (
 
     <div>
-      <Header />
+      <Header/>
       <Technologies/>
+      <Footer/>
     </div>
   );
 }
---
+++ 01-first-project/react-kabzda-1/src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+
+    return ( <div>
+      It-kamasutra.com footer
+    </div>);
+  }
+  
+  export default Footer;
