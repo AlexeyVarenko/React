50faea118cc7866daef6400257f902706369cc54
AlexeyVarenko <alexey_varenko@mail.ru>
2022-02-09 12:26:02 +0300

    Lesson 16. Props

--- 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -10,13 +10,9 @@ const MyPosts = () => {
       <button>Add post</button>
     </div>
     <div className={s.posts}>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-    </div>
+      <Post message='Hi, how are you?'/>
+      <Post message="It's my first post"/>
+      </div>
   </div>
 }
 
--- 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/Post/Post.jsx
+++ 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
   return <div className={s.item}>
     <img src='https://www.vokrug.tv/pic/person/2/b/f/4/2bf448098b7badf3b37e87c510da29bc.jpeg' />
-    post 1
+    { props.message }
     <div>
       <span>like</span>
     </div>
