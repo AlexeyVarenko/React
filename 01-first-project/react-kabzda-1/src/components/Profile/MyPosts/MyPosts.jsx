339a9ade51a84024d0ee9a30527e4d778d129073
AlexeyVarenko <alexey_varenko@mail.ru>
2022-02-11 15:26:14 +0300

    Lesson 23
    Props for Dialogs

--- 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,16 +3,20 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
-  return <div>
+  return <div className={s.postsBlock}>
     My Posts
     <div>
-      <textarea></textarea>
-      <button>Add post</button>
+      <div>
+        <textarea></textarea>
+      </div>
+      <div>
+        <button>Add post</button>
+      </div>
     </div>
     <div className={s.posts}>
-      <Post message='Hi, how are you?' like='20'/>
-      <Post message="It's my first post" like='15'/>
-      </div>
+      <Post message='Hi, how are you?' like='20' />
+      <Post message="It's my first post" like='15' />
+    </div>
   </div>
 }
 
