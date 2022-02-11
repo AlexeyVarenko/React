9e956aed177e371d0d6b7ea49e2e03f7c195be85
AlexeyVarenko <alexey_varenko@mail.ru>
2022-02-11 16:20:21 +0300

    Lesson 24
    UI
    BLL

--- 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ 01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,17 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+  let postData=[
+    {id:1, message: 'Hi, how are you?', likeCount: 12},
+    {id:2, message: "It's my first post", likeCount: 11},
+]
+
+
+
+
+
+
   return <div className={s.postsBlock}>
     My Posts
     <div>
@@ -14,8 +25,8 @@ const MyPosts = () => {
       </div>
     </div>
     <div className={s.posts}>
-      <Post message='Hi, how are you?' like='20' />
-      <Post message="It's my first post" like='15' />
+      <Post message={postData[0].message} like={postData[0].likeCount} />
+      <Post message={postData[1].message} like={postData[1].likeCount} />
     </div>
   </div>
 }
