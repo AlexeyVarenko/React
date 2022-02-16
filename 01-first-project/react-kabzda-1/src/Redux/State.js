f8db2ba6e9a24bff68e2b6b96d5a8fd4af261559
AlexeyVarenko <alexey_varenko@mail.ru>
2022-02-16 15:32:43 +0300

    Lesson 33
    add render.js

--- 01-first-project/react-kabzda-1/src/Redux/State.js
+++ 01-first-project/react-kabzda-1/src/Redux/State.js
@@ -1,3 +1,4 @@
+import { rerenderEntireTree } from "../render";
 
   let state={
 
@@ -34,14 +35,13 @@
 
 
   export let addPost=(postMessage)=>{
-    debugger;
     let newPost={
       id:5,
       message:postMessage,
       likeCount: 0,
     };
-
     state.profilePage.posts.push(newPost);
+    rerenderEntireTree(state);
   }
 
   export default state;
--- 01-first-project/react-kabzda-1/src/index.js
+++ 01-first-project/react-kabzda-1/src/index.js
@@ -1,23 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
-import state from './Redux/State'
-import {addPost} from './Redux/State'
-import { BrowserRouter } from 'react-router-dom';
+import { rerenderEntireTree } from './render';
+import state from './Redux/State';
 
-//addPost('SamuraiJS.COM')
 
-ReactDOM.render(
+rerenderEntireTree(state);
 
-  <React.StrictMode>
-    <BrowserRouter>
-      <App state={state} addPost={addPost} />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
