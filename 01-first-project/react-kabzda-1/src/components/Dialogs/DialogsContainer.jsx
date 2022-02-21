6b261757dd5c654f75a45bf460e593f753c8c3c4
AlexeyVarenko <alexey_varenko@mail.ru>
2022-02-21 18:06:28 +0300

    Lesson 43
    container component

---
+++ 01-first-project/react-kabzda-1/src/components/Dialogs/DialogsContainer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { sendMessageCreator, updateNewMessageBodyCreator } from '../../Redux/dialogs-reducer';
+import DialogItem from './DialogItem/DialogItem';
+import Dialogs from './Dialogs';
+import Message from './Message/Message';
+
+
+
+
+const DialogsContainer = (props) => {
+
+    let state = props.store.getState().dialogsPage;
+
+
+    let onSendMessageClick = () => {
+        props.store.dispatch(sendMessageCreator());
+    };
+
+    let onNewMessageChange = (body) => {
+        props.store.dispatch(updateNewMessageBodyCreator(body));
+    };
+
+    return (
+        <Dialogs updateNewMessageBody={onNewMessageChange}
+         sendMessage={onSendMessageClick}
+         dialogsPage={state}/>
+    )
+}
+
+export default DialogsContainer;
