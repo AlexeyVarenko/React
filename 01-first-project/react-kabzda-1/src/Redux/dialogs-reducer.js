dab51fc1764e08889a3108ab452ac546a32cd3dc
AlexeyVarenko <alexey_varenko@mail.ru>
2022-02-23 18:08:26 +0300

    Lesson 48
    Copy in DialogReducer

--- 01-first-project/react-kabzda-1/src/Redux/dialogs-reducer.js
+++ 01-first-project/react-kabzda-1/src/Redux/dialogs-reducer.js
@@ -1,50 +1,52 @@
-const UPDATE_NEW_MESSAGE_BODY='UPDATE_NEW_MESSAGE_BODY';
-const SEND_MESSAGE='SEND_MESSAGE';
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
+const SEND_MESSAGE = 'SEND_MESSAGE';
 
-let initialState={
-    messages: [
-      { id: 1, message: 'Hi' },
-      { id: 2, message: 'How is your it-kamasutra?' },
-      { id: 3, message: 'Yo' },
-      { id: 4, message: 'Yo' },
-      { id: 5, message: 'Yo' },
-    ],
-    dialogs: [
-      { id: 1, name: 'Dimych' },
-      { id: 2, name: 'Andrey' },
-      { id: 3, name: 'Sveta' },
-      { id: 4, name: 'Sasha' },
-      { id: 5, name: 'Victor' },
-      { id: 6, name: 'Valera' },
-    ],
-    newMessageBody: ""
+let initialState = {
+  messages: [
+    { id: 1, message: 'Hi' },
+    { id: 2, message: 'How is your it-kamasutra?' },
+    { id: 3, message: 'Yo' },
+    { id: 4, message: 'Yo' },
+    { id: 5, message: 'Yo' },
+  ],
+  dialogs: [
+    { id: 1, name: 'Dimych' },
+    { id: 2, name: 'Andrey' },
+    { id: 3, name: 'Sveta' },
+    { id: 4, name: 'Sasha' },
+    { id: 5, name: 'Victor' },
+    { id: 6, name: 'Valera' },
+  ],
+  newMessageBody: ""
 
-  };
+};
 
- const dialogsReducer = (state=initialState, action)=>{
-     switch(action.type){
-         case UPDATE_NEW_MESSAGE_BODY: {
-           let stateCopy={...state};
+const dialogsReducer = (state = initialState, action) => {
 
-           stateCopy.newMessageBody = action.body;
-            return stateCopy;
-          }
-        case SEND_MESSAGE:{
-            let stateCopy={...state};
-            stateCopy.messages = [...state.messages];
-            let body=state.newMessageBody
-            stateCopy.newMessageBody = '';
-            stateCopy.messages.push({ id: 6, message: body });
-            return stateCopy;
-          }
-        default:
-            return state;
 
-     }
+  switch (action.type) {
+    
+    case UPDATE_NEW_MESSAGE_BODY:
+      return {
+        ...state,
+        newMessageBody:action.body,
+      };
+
+    case SEND_MESSAGE:
+      let body = state.newMessageBody
+      return {
+        ...state,
+        newMessageBody:'',
+        messages: [...state.messages, { id: 6, message: body }],
+      };
+    default:
+      return state;
+
+  }
 }
 
-export const sendMessageCreator=()=>({ type: SEND_MESSAGE });
-export const updateNewMessageBodyCreator=(body)=>({ type: UPDATE_NEW_MESSAGE_BODY, body:body });
+export const sendMessageCreator = () => ({ type: SEND_MESSAGE });
+export const updateNewMessageBodyCreator = (body) => ({ type: UPDATE_NEW_MESSAGE_BODY, body: body });
 
 
 export default dialogsReducer
