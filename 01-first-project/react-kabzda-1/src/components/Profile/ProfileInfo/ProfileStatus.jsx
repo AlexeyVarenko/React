8bbc9b08a44e260f57c8f733d5637eb8245c4742
AlexeyVarenko <alexey_varenko@mail.ru>
2022-03-08 15:41:18 +0300

    Lesson 71
    Fail

--- 01-first-project/react-kabzda-1/src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ 01-first-project/react-kabzda-1/src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -3,37 +3,35 @@ import s from './ProfileInfo.module.css';
 
 
 class ProfileStatus extends React.Component {
+    
     state = {
         editMode: false,
     }
 
-    activateEditMode () {
-
+    activateEditMode= ()=> {
         this.setState({
             editMode: true,
         })
     }
 
-    deactivateEditMode () {
-
+    deactivateEditMode = ()=>{
         this.setState({
             editMode: false,
         })
     }
 
-
+    
     render() {
-
         return (
             <div>
                 {!this.state.editMode &&
                     <div>
-                        <span onDoubleClick={ this.activateEditMode.bind(this) }>{this.props.status}</span>
+                        <span onDoubleClick={ this.activateEditMode }>{this.props.status}</span>
                     </div>
                 }
                 {this.state.editMode &&
                     <div>
-                        <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status} />
+                        <input autoFocus={true} onBlur={this.deactivateEditMode} value={this.props.status} />
                     </div>
                 }
             </div>
