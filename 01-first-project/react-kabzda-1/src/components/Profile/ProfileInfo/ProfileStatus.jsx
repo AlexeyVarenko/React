81bbfe2ab7dec8c349e211f855b40e8a882f5686
AlexeyVarenko <alexey_varenko@mail.ru>
2022-03-09 14:39:08 +0300

    Lesson 74
    componentDidUpdate

--- 01-first-project/react-kabzda-1/src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ 01-first-project/react-kabzda-1/src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -28,7 +28,14 @@ class ProfileStatus extends React.Component {
             status: e.currentTarget.value
         })
     }
-
+    componentDidUpdate(prevProps, prevState) {
+       if (prevProps.status !== this.props.status){
+           this.setState({
+               status:this.props.status
+           })
+       }
+       console.log('componentDidUpdate');
+    }
     
     render() {
         return (
