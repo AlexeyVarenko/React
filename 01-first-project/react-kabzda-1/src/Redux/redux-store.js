53b620ff8c29ee1da4d313a353a4885c2c2acf50
AlexeyVarenko <alexey_varenko@mail.ru>
2022-03-11 16:05:21 +0300

    Lesson 75
    redux-form (-)

--- 01-first-project/react-kabzda-1/src/Redux/redux-store.js
+++ 01-first-project/react-kabzda-1/src/Redux/redux-store.js
@@ -5,6 +5,7 @@ import profileReducer from "./profile-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import thunkMiddleware from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form'
 
 
 let redusers=combineReducers({
@@ -13,6 +14,7 @@ let redusers=combineReducers({
   sidebar: sidebarReducer,
   usersPage: usersReducer,
   auth: authReducer,
+  form: formReducer,
 })
 
 let store=createStore(redusers, applyMiddleware(thunkMiddleware));
