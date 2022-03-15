aad2a4698ffa27677b7eeef339f83cdcc6a7b054
AlexeyVarenko <alexey_varenko@mail.ru>
2022-03-15 16:12:35 +0300

    Lesson 84
    Hook, useState

--- 01-first-project/react-kabzda-1/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ 01-first-project/react-kabzda-1/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Preloader from '../../common/Preloader/Preloader';
 import s from './ProfileInfo.module.css';
 import ProfileStatus from './ProfileStatus'
+import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
 const ProfileInfo = (props) => {
 
@@ -15,7 +16,7 @@ const ProfileInfo = (props) => {
       <img src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300' />
     </div>
     <div className={s.descriptionBlock}>
-      <ProfileStatus status ={props.status} updateStatus={props.updateStatus}/>
+      <ProfileStatusWithHooks status ={props.status} updateStatus={props.updateStatus}/>
       <img src={props.profile.photos.large}/>
        <div>{props.profile.aboutMe}</div>
        <div>{props.profile.fullName}</div>
