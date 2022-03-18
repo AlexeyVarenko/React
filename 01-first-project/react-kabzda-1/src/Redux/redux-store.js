7b89698518c6b2f4d42a024e506866bfe8134b66
AlexeyVarenko <alexey_varenko@mail.ru>
2022-03-18 15:09:26 +0300

    Lesson 91
    Chrome extensions

--- 01-first-project/react-kabzda-1/src/Redux/redux-store.js
+++ 01-first-project/react-kabzda-1/src/Redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import authReducer from "./auth-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import profileReducer from "./profile-reducer";
@@ -19,8 +19,9 @@ let redusers=combineReducers({
   app:appReducer
 })
 
-let store=createStore(redusers, applyMiddleware(thunkMiddleware));
-window.store=store;
+ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ const store = createStore(redusers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+window.__store__=store;
 
 
 export default store
