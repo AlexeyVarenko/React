34fabb9e06fe0ff3f17e550398107889a868fc94
AlexeyVarenko <alexey_varenko@mail.ru>
2022-03-23 12:55:25 +0300

    Lesson 98
    captcha

--- 01-first-project/react-kabzda-1/src/components/Login/login.jsx
+++ 01-first-project/react-kabzda-1/src/components/Login/login.jsx
@@ -9,12 +9,15 @@ import style from '../common/FormsControls/FormControl.module.css'
 
 
 
-const LoginForm = ({handleSubmit, error}) => {
+const LoginForm = ({handleSubmit, error, captchaUrl}) => {
     return (
         <form onSubmit={handleSubmit}>
             {createField("Email", "email", [required], Input)}
             {createField("Password", "password", [required], Input, {type:"password"})}
             {createField(null, "rememberMe", [], Input, {type:"checkbox"}, "remember me")}
+
+            {captchaUrl && <img src={captchaUrl}/>}
+            {captchaUrl && createField("Symbols from image", "captcha", [required], Input, {} )}
 
             {error && <div className={style.formSummaryError}>
                 {error}
@@ -31,7 +34,7 @@ const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
 
 const Login = (props) => {
     const onSubmit=(formData)=>{
-        props.login(formData.email, formData.password, formData.rememberMe);
+        props.login(formData.email, formData.password, formData.rememberMe, formData.captcha);
     }
 
     if(props.isAuth){
@@ -40,11 +43,12 @@ const Login = (props) => {
 
     return <div>
         <h1>Login</h1>
-        <LoginReduxForm onSubmit={onSubmit} />
+        <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl}/>
     </div>
 }
 
 const mapStateToProps=(state)=>({
+    captchaUrl:state.auth.captchaUrl,
     isAuth: state.auth.isAuth
 })
 
